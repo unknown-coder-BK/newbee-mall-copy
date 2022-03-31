37b42ab23399054f6975ff3a8c6ea77dc6ba0d3b
ZhangBen <zb85760059>
2022-03-31 14:29:41 +0800

    拦截器配置

---
+++ src/main/java/ltd/newbee/mall/config/properties/WebConfig.java
@@ -0,0 +1,106 @@
+package ltd.newbee.mall.config.properties;
+
+import ltd.newbee.mall.interceptor.MallLoginValidateInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${local.uploadDir}")
+    private String uploadDir;
+
+//    @Resource
+//    private RepeatSubmitInterceptor repeatSubmitInterceptor;
+
+////    @Value("${wayn.viewModel}")
+////    private boolean viewModel;
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addRedirectViewController("/", "/index");
+//    }
+//
+//
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /** 本地文件上传路径 */
+//        registry.addResourceHandler("/upload/**").addResourceLocations("file:" + uploadDir + "/");
+        registry.addResourceHandler("/goods-img/**").addResourceLocations("file:" + uploadDir + "/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MallLoginValidateInterceptor())
+                  .excludePathPatterns("/")
+                  .excludePathPatterns("/index")
+                  .excludePathPatterns("/login")
+                  .excludePathPatterns("/common/**")
+                  .excludePathPatterns("/admin/**")
+                  .excludePathPatterns("/goods-img/**");
+//                .excludePathPatterns("/login")
+//                .excludePathPatterns("/logout")
+//                .excludePathPatterns("/register")
+//                .excludePathPatterns("/index")
+//                .excludePathPatterns("/seckill/list")
+//                .excludePathPatterns("/search")
+//                .excludePathPatterns("/goods/**")
+//                .excludePathPatterns("/shopCart/getUserShopCartCount")
+//                .excludePathPatterns("/seckill/detail/*")
+//                .excludePathPatterns("/seckill/time/now")
+//                .excludePathPatterns("/seckill/*/exposer")
+//                .excludePathPatterns("/upload/**")
+//                .excludePathPatterns("/mall/**")
+
+
+
+//        // 添加一个拦截器，拦截以/admin为前缀的url路径（后台登陆拦截）
+//        registry.addInterceptor(new AdminLoginInterceptor())
+//                .addPathPatterns("/admin/**")
+//                .excludePathPatterns("/admin/login")
+//                .excludePathPatterns("/admin/dist/**")
+//                .excludePathPatterns("/admin/plugins/**");
+//
+
+//        registry.addInterceptor(repeatSubmitInterceptor)
+//                .addPathPatterns("/**");
+//
+//        registry.addInterceptor(new AdminViewModelInterceptor(viewModel))
+//                .addPathPatterns("/admin/**")
+//                .excludePathPatterns("/admin/login")
+//                .excludePathPatterns("/admin")
+//                .excludePathPatterns("/admin/statistics")
+//                .excludePathPatterns("/admin/goods")
+//                .excludePathPatterns("/admin/goods/add")
+//                .excludePathPatterns("/admin/goods/edit/*")
+//                .excludePathPatterns("/admin/goods/list")
+//                .excludePathPatterns("/admin/users")
+//                .excludePathPatterns("/admin/users/list")
+//                .excludePathPatterns("/admin/carousels")
+//                .excludePathPatterns("/admin/carousels/list")
+//                .excludePathPatterns("/admin/indexConfigs")
+//                .excludePathPatterns("/admin/indexConfigs/list")
+//                .excludePathPatterns("/admin/categories")
+//                .excludePathPatterns("/admin/categories/list")
+//                .excludePathPatterns("/admin/orders")
+//                .excludePathPatterns("/admin/orders/list")
+//                .excludePathPatterns("/admin/coupon")
+//                .excludePathPatterns("/admin/coupon/list")
+//                .excludePathPatterns("/admin/seckill")
+//                .excludePathPatterns("/admin/seckill/list")
+//                .excludePathPatterns("/admin/profile")
+//                .excludePathPatterns("/admin/logout")
+//                .excludePathPatterns("/admin/dist/**")
+//                .excludePathPatterns("/admin/plugins/**");
+    }
+
+}
---
+++ src/main/java/ltd/newbee/mall/interceptor/MallLoginValidateInterceptor.java
@@ -0,0 +1,34 @@
+package ltd.newbee.mall.interceptor;
+
+import ltd.newbee.mall.constant.Constants;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MallLoginValidateInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        /**拦截没有登录的key 进行无线重定向*/
+        if (null == request.getSession().getAttribute(Constants.MALL_USER_SESSION_KEY)) {
+            response.sendRedirect(request.getContextPath() + "/login?destPath=" + request.getServletPath());
+            return false;
+        }
+        /**有key不进行拦截操作*/
+        else {
+            return true;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
